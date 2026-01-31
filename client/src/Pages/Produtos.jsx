fe29f25ae3dccdad6631b20d6d9e1270082da8c0
MarioAugustoTecnologia <mariopisanibtu@gmail.com>
2026-01-31 19:03:53 -0300

    update

--- client/src/Pages/Produtos.jsx
+++ client/src/Pages/Produtos.jsx
@@ -112,7 +112,6 @@ const Produtos = ({ produto }) => {
          window.location.href = '/login';
       }
 
-
    }
 
    const usuario = localStorage.getItem('usuario');
@@ -122,29 +121,34 @@ const Produtos = ({ produto }) => {
 
       <div className="">
 
-          <nav className="d-flex bg-secondary text-white " style={{height:20}}>               
-
+         <nav className="d-flex bg-secondary text-white " style={{ height: 20 }}>
 
             <div className="d-flex justify-content-center">
-               <a href="" className="navbar-brand text-white" style={{fontSize:'15px'}}>Pesquisar:</a>
-               <input type="search" style={{ margin: "0 12px", width: "130px", height: '25px'}} className="form-control rounded-0" value={buscanome} onChange={(e) => setBuscaNome(e.target.value)}  />
-                         
-            </div>
-            <Link to="/carrinho" className="navbar-link fs-5 text-white" style={{margin:'-5px'}}><BsCart style={{margin:'10px', fontSize:'20px'}} />{cart.length} </Link>
-         </nav>                  
-    
-         <div className="d-flex justify-content-between bg-secondary py-3 px-5 text-white">
-            <h5 className="text-white" id="user" style={{marginBlock:'10px', fontSize:'15px'}} > {usuario}</h5>
-            <Link className="navbar-link fs-5 text-white"><a style={{fontSize:'15px', margin:'-650px'}} onClick={SairdaConta}>Sair:</a></Link>
-            <div className="">
-               <Link to="" style={{ color: 'white' , fontSize:'13px', margin:'-1680px'}} onClick={ComparaCadastro}>Não Possui Conta ? Criar Conta:</Link><br />
-               <Link to="" onClick={ComparaLogin} style={{ color: 'white', fontSize:'13px', margin:'-1680px' }}>Já Possui Conta ? - Faça o Login: </Link>
+
+               <a href="" className="navbar-brand text-white" style={{ fontSize: '15px', marginBlock: '10px' }}>Pesquisar:</a>
+               <input type="search" style={{ margin: "0 12px", width: "130px", height: '25px', marginBlock: '10px' }} className="form-control rounded-0" value={buscanome} onChange={(e) => setBuscaNome(e.target.value)} />
+
 
             </div>
-            
+
+            <Link to="/carrinho" className="navbar-link fs-5 text-white" style={{ margin: '-5px' }}><BsCart style={{ margin: '10px', fontSize: '20px' }} />{cart.length} </Link> <br />
+
+         </nav>
+
+         <div className="d-flex justify-content-between bg-secondary py-3 px-5 text-white">
+
+            <Link className="navbar-link fs-5 text-white"><a style={{ fontSize: '15px', margin: '200px' }}></a></Link>
+            <div className="">
+               <Link to="" style={{ color: 'white', fontSize: '13px', margin: '-1680px' }} onClick={ComparaCadastro}>Não Possui Conta ? Criar Conta:</Link><br />
+               <Link to="" onClick={ComparaLogin} style={{ color: 'white', fontSize: '13px', margin: '-1680px' }}>Já Possui Conta ? - Faça o Login: </Link>
+
+            </div>
 
          </div>
-
+         <div className="d-flex bg-secondary text-white" style={{ height: 40 }}>
+            <h6 style={{ color: 'white', margin: '0 20px' }} id="user" >{usuario}</h6>
+            <Link style={{ color: 'white', fontSize: '15px', margin: '0 50px' }} onClick={SairdaConta}>Sair:</Link>
+         </div>
          <div className='container mt-5'>
 
 
