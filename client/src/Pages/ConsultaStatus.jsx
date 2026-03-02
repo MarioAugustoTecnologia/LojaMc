2f38efe3add037f1d18069bf0139e4adef2dbfa2
MarioAugustoTecnologia <mariopisanibtu@gmail.com>
2026-03-02 12:29:24 -0300

    update

--- client/src/Pages/ConsultaStatus.jsx
+++ client/src/Pages/ConsultaStatus.jsx
@@ -34,7 +34,6 @@ const ConsultaStatus = ({item}) => {
     var table = pedido.filter(item => item.status.toLowerCase().includes(buscarap))
 
 
-
   useEffect(() => {
     fetch("https://lojamcserver.onrender.com/pedidos").then((res) => {
 
@@ -96,7 +95,7 @@ const ConsultaStatus = ({item}) => {
 
         <div className="d-flex">
              <a href="" className="navbar-brand text-white" style={{fontSize:"16px", margin:'-5px'}}>Pesquisar:</a>
-             <input type="search" style={{ margin: "0 15px", width: "210px", height: '25px' }} placeholder="Digite o status do Pedido:" className="form-control rounded-0" value={buscapedido} onChange={(e) => setBuscaPedido(e.target.value)} />
+             <input type="search" style={{ margin: "0 15px", width: "230px", height: '25px' }} placeholder="Digite o status do Pedido:" className="form-control rounded-0" value={buscapedido} onChange={(e) => setBuscaPedido(e.target.value)} />
         </div>  
 
 
