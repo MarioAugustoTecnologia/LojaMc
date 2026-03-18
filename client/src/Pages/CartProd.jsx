a31b5bece9393a65c885591c859e16bcc806c4ff
MarioAugustoTecnologia <mariopisanibtu@gmail.com>
2026-03-18 16:50:28 -0300

    update

--- client/src/Pages/CartProd.jsx
+++ client/src/Pages/CartProd.jsx
@@ -488,7 +488,7 @@ const total = "Total:  R$" + (totalpreco(cart)).toFixed(2);
               </div>
               <br />
               <div className='detail ms-4'>
-                <input type="text" id="desc" readOnly style={{ border: 'none', width: '500px', fontSize: '17px' }} value={descricao} onChange={e => descchange(e.target.value)} /> <br />
+                <input type="text" id="desc" readOnly style={{ border: 'none', width: '450px', fontSize: '17px' }} value={descricao} onChange={e => descchange(e.target.value)} /> <br />
                 <input type="text" readOnly style={{ fontWeight: 'bold', color: 'DarkMagenta', border: 'none', fontSize: '20px', width: '160px' }} value={"R$" + prec} onChange={e => precochange(e.target.value)}></input> <br />
                 <input type="text" readOnly style={{ fontWeight: 'bold', color: 'Green', border: 'none', fontSize: '20px', width: '200px' }} value={statusprod} id="statusprod" onChange={e => statuschange(e.target.value)} /> <br />
                 <button type="submit" className="btn btn-success" onClick={comprar} id="botao">Comprar: </button>
